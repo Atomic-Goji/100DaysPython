2001d2673e184ff3f6f00bfbed4770e2ea6e276d
Atomic-Goji <adarshff2009@gmail.com>
2026-03-19 22:35:38 +0530

    this is a simple calculator program in python which can perform basic arithmetic operations like addition, subtraction, multiplication and division. The program takes two numbers as input from the user and then asks the user to choose the operation they want to perform. Based on the user's choice, the program performs the selected operation and displays the result. The program also includes error handling to manage cases where the user inputs invalid data or tries to divide by zero.

---
+++ Simple_Calculator.py
@@ -0,0 +1,10 @@
+# A simple Calculator
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("The sum of the two numbers is: ", a+b)
+print("The difference of the two numbers is: (a-b)", a-b)
+print("The product of the two numbers is: ", a*b)
+print("The division of the two numbers is: (a/b)", a/b)
+
