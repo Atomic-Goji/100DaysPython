736f8dbe2e1a8f9b2c7a9b88e3474b57df1a3a58
Atomic-Goji <adarshff2009@gmail.com>
2026-03-19 19:54:28 +0530

    initial commit

---
+++ T001.py
@@ -0,0 +1,23 @@
+# print("Hello World" , 835, 45,345, 45.34, sep="__") 
+# print("""Never gonna give you up
+# Never gonna let you down
+# Never gonna run around and desert you""")
+# # this song is by Rick Astley 
+# # this program is written by eddie 
+
+# print("Never gonna give you up \nNever gonna let you down \nNever gonna run around and desert you")
+
+
+a=12
+b="Eddie"
+print(str(a)+b)
+print(type(a))
+print(type(b))
+
+
+z=complex(2,3)
+print(z)
+print(type(z))
+print(z.real)
+print(z.imag)
+
